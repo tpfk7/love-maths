ba91dada510a1e69f219eaa5327b2165282fb93d
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-02-20 23:02:27 +0000

    Add multiplication game code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -30,7 +30,10 @@ function runGame(gameType){
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
-    }else{
+    }else if (gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
+    }
+    else{
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting`;
     }
@@ -68,7 +71,10 @@ function calculateCorrectAnswer(){
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"];
-    } else{
+    } else if (operator === "x"){
+        return [operand1 * operand2, "multiply"];
+    }
+    else{
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -102,6 +108,9 @@ function displayAdditionQuestion(operand1, operand2){
 function displaySubtractQuestion(){
     
 }
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
     
 }
