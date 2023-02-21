3b25d2637b80f2b7cd0c62cf0570e06d3b708e37
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-02-21 15:13:17 +0000

    Add favicon, and final tidy ups for the user

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -16,6 +16,12 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     runGame("addition");
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+    })
+
 
 })
 /**
@@ -23,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
